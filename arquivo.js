f82328418077299887dbba026532d2b815951045
Marina Hedlund Jardim <84291180+M705-bit@users.noreply.github.com>
2025-06-18 15:18:16 -0300

    Update arquivo.js

--- arquivo.js
+++ arquivo.js
@@ -1,5 +1,12 @@
 const letras = document.querySelectorAll(".flex-item");
+const botao 
 for (let letra = 0; letra < letras.length; letra++) {
-	    alert(letras[0]);
+	    letras[0].textContent;
+		//alvo.addEventListener(type,listener[, option]);
+		letras.addEventListener("click", findWord());
 }
+ alert(letras[0].textContent);
 
+ function findWord(){
+	if l
+ }
